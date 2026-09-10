15b16cf2dd6703dd2fb54f05c18823ebf31a32a7
Taniya Dhiman <taniyadhiman738@gmail.com>
2026-09-10 21:57:55 +0530

    Update frontend API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://rfq-marketplace-v5k8.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
